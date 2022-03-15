468df17f9c0d4f7ef1f22ea699459c2af9933402
pengxy <pengxy@zenmen.com>
2022-03-15 20:22:56 +0800

    rename

---
+++ day20_webpack使用/01_webpack_test/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+    //编译模式
+    mode:'development',
+    entry:path.join(__dirname,'./src/index.js'),
+    output:{
+        path:path.join(__dirname,'./dist'),
+        filename:'bundle.js'
+    }
+}
